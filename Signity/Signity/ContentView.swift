7ac2a4c79a285eb7d050d3bc8dc6bb25111af3da
ardnt88 <i.komang04@ui.ac.id>
2021-08-02 17:43:52 +0700

    Update ContentView.swift
    change to ultralight

--- Signity/Signity/ContentView.swift
+++ Signity/Signity/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, Team 11 Lucu Onboarding")
-            .fontWeight(.bold)
+            .fontWeight(.ultraLight)
             .padding()
     }
 }
