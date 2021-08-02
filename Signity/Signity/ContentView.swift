97d04bbdaf7166ea3bcd2618a99ecd5e70a42670
charlieperdana <charlie.siagian@gmail.com>
2021-08-02 16:28:29 +0700

    structure

---
+++ Signity/Signity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Signity
+//
+//  Created by charlie siagian on 02/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Team 11")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
