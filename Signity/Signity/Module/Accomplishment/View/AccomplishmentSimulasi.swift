0aeea841a55042fd54e0669136f67005fecc6058
Devina Ranlyca <devinar00@gmail.com>
2021-08-06 12:19:09 +0700

    Accomplishment Simulasi + Tiru

---
+++ Signity/Signity/Module/Accomplishment/View/AccomplishmentSimulasi.swift
@@ -0,0 +1,20 @@
+//
+//  AccomplishmentSimulasi.swift
+//  Signity
+//
+//  Created by Devina Ranlyca on 06/08/21.
+//
+
+import SwiftUI
+
+struct AccomplishmentSimulasi: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccomplishmentSimulasi_Previews: PreviewProvider {
+    static var previews: some View {
+        AccomplishmentSimulasi()
+    }
+}
---
+++ Signity/Signity/Module/Accomplishment/View/AccomplishmentTiru.swift
@@ -0,0 +1,20 @@
+//
+//  AccomplishmentTiru.swift
+//  Signity
+//
+//  Created by Devina Ranlyca on 06/08/21.
+//
+
+import SwiftUI
+
+struct AccomplishmentTiru: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccomplishmentTiru_Previews: PreviewProvider {
+    static var previews: some View {
+        AccomplishmentTiru()
+    }
+}
