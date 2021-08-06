77cb721d5609cfc8e817503c756b1d15239aafa0
Devina Ranlyca <devinar00@gmail.com>
2021-08-06 12:19:34 +0700

    Accomplishment Simulasi + Tiru

--- Signity/Signity/Module/Accomplishment/View/AccomplishmentSimulasi.swift
+++ Signity/Signity/Module/Accomplishment/View/AccomplishmentSimulasi.swift
@@ -8,10 +8,80 @@
 import SwiftUI
 
 struct AccomplishmentSimulasi: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    
+    var btnBack : some View { Button(action: {
+        
+        self.presentationMode.wrappedValue.dismiss()
+        }) {
+            HStack {
+            Image("backBtnWhite")
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.white)
+
+            }
+        }
     }
+    
+    var body: some View {
+        NavigationView{
+        
+        ZStack{
+            
+            Color(#colorLiteral(red: 0.2533461452, green: 0.3034159541, blue: 0.8465253711, alpha: 1)).ignoresSafeArea(.all)
+            
+            VStack{
+                
+                    
+                    Text("Simulasi Pelajaran Selesai!")
+                        .font(.system(size: 32, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .multilineTextAlignment(.center)
+                    
+                Spacer()
+                    
+                    Image("LatihanSimulasi_Badge")
+                        .offset(y:70)
+                    
+                Spacer()
+                    
+                    
+                    Image("bottomCircle")
+                        .offset(y:148)
+                
+                  
+                Spacer()
+                    
+                    
+                    Button(action: {
+                        print("Go to simulasi practice page")
+                        
+                                    },
+                    label: {
+                        Text("Mulai Latihan Simulasi")
+                        .foregroundColor(.white)
+                        .font(.system(size:22, design:.rounded))
+                        .fontWeight(.bold)
+                        .frame(width: 358, height: 50)
+                            .background(Color("MainPurple"))
+                        .cornerRadius(13)
+                            })
+            }
+
+    }
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: btnBack)
+            
+            
 }
+        
+    
+}
+}
+
 
 struct AccomplishmentSimulasi_Previews: PreviewProvider {
     static var previews: some View {
