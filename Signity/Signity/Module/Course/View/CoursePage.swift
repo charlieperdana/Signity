4036d4bcbb6404c4c7fceb6026d10c76ca5cdeda
Andrean Lay <andreanlay1@gmail.com>
2021-08-22 20:35:15 +0700

    Add message when app fial to fetch data (empty)

--- Signity/Signity/Module/Course/View/CoursePage.swift
+++ Signity/Signity/Module/Course/View/CoursePage.swift
@@ -9,65 +9,71 @@ import SwiftUI
 
 struct CoursePage: View {
     @State private var showSettings = false
-    
+
     @StateObject var viewModel = CourseViewModel()
-    
+
     @State var refreshID = UUID()
     
     var body: some View {
-        ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false) {
-            VStack(alignment:.leading){
-                Group {
-                    HStack(alignment: .center){
-                        Text("Pelajaran")
-                            .modifier(SignityLargeTitle(color: .text))
-                        
-                        Spacer()
-                        
-                        Button(action: {
-                            self.showSettings = true
-                        }) {
-                            Image(systemName: "gear")
-                                .resizable()
-                                .frame(width: 35, height: 35)
-                        }
-                        .foregroundColor(Color("Button"))
-                    }
-                    
-                }
-                .padding(.horizontal)
-                .padding(.top, 50)
-                
-                Text("BISINDO \(viewModel.currentRegion)")
-                    .modifier(SignityHeadlineThin(color: .text))
-                    .padding([.leading, .bottom])
-                
-                
-                Spacer()
-                
-                
+        if viewModel.currentModules.isEmpty {
+            Text("No data found. This may be caused by corrupted data in your installation. Please reinstall Signity.")
+                .multilineTextAlignment(.center)
+                .padding()
+        } else {
+            ScrollView(/*@START_MENU_TOKEN@*/.vertical/*@END_MENU_TOKEN@*/, showsIndicators: false) {
                 VStack(alignment:.leading){
-                    ForEach(viewModel.currentModules, id: \.self) { group in
-                        Section(header: Text(group.name)
-                                    .modifier(SignitySubtitle(color: .text))) {
-                            ForEach(group.categories, id: \.self) { item in
-                                CardViewCourse(category: item, proficiency: viewModel.regionProficiency)
+                    Group {
+                        HStack(alignment: .center){
+                            Text("Pelajaran")
+                                .modifier(SignityLargeTitle(color: .text))
+                            
+                            Spacer()
+                            
+                            Button(action: {
+                                self.showSettings = true
+                            }) {
+                                Image(systemName: "gear")
+                                    .resizable()
+                                    .frame(width: 35, height: 35)
+                            }
+                            .foregroundColor(Color("Button"))
+                        }
+                        
+                    }
+                    .padding(.horizontal)
+                    .padding(.top, 50)
+                    
+                    Text("BISINDO \(viewModel.currentRegion)")
+                        .modifier(SignityHeadlineThin(color: .text))
+                        .padding([.leading, .bottom])
+                    
+                    
+                    Spacer()
+                    
+                    
+                    VStack(alignment:.leading){
+                        ForEach(viewModel.currentModules, id: \.self) { group in
+                            Section(header: Text(group.name)
+                                        .modifier(SignitySubtitle(color: .text))) {
+                                ForEach(group.categories, id: \.self) { item in
+                                    CardViewCourse(category: item, proficiency: viewModel.regionProficiency)
+                                }
                             }
                         }
                     }
+                    .id(self.refreshID)
+                    .padding(.horizontal)
                 }
-                .id(self.refreshID)
-                .padding(.horizontal)
             }
-        }
-        
-        .navigationBarHidden(true)
-        .sheet(isPresented: $showSettings) {
-            SettingsView()
-        }
-        
-        .onAppear {
-            self.refreshID = UUID()
+            
+            .navigationBarHidden(true)
+            .sheet(isPresented: $showSettings) {
+                SettingsView()
+            }
+            
+            .onAppear {
+                self.refreshID = UUID()
+            }
         }
     }
 }
